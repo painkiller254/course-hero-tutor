ef02cb738baaacf3d7577c5a6e5bf0e605e1a7fb
painkiller254 <gachungasamuel53@gmail.com>
2022-03-15 08:07:18 +0300

    h

---
+++ ho.c
@@ -0,0 +1,18 @@
+#inclued <stdio.h>
+
+int main(){
+	char clock[10];
+	int price;
+
+	printf("Enter a clock : ");
+	scanf("%s", &clock);
+
+	printf("enter the clock price: ");
+	scanf("%d", &price); 
+
+	printf("the name entered is: %s\n", name);
+
+	if price > 
+
+return 0;
+}
