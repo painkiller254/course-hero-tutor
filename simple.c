54e5c1374d7fcaba3db873dd6aadd97c7515d056
painkiller254 <gachungasamuel53@gmail.com>
2022-03-15 08:05:26 +0300

    dd

---
+++ simple.c
@@ -0,0 +1,17 @@
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Please Enter a choice from below (1-3):");  
+      Console.WriteLine("-------------------------------------");  
+      Console.WriteLine("1. Enter student details");  
+      Console.WriteLine("2. Show Average marks and grades");  
+      Console.WriteLine("3. Logout");  
+    }
+  }
+}
+
